a57d79522bc89e63b366ab1499af6c8f0d5b24da
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-07 16:09:07 +0200

    add js

--- script.js
+++ script.js
@@ -2,6 +2,4 @@ var nome = prompt("Scrivi il tuo nome");
 var cognome = prompt("Scrivi il tuo cognome");
 var colore = prompt("Scrivi il tuo colore preferito");
 
-
-
 document.getElementById("myH1").innerHTML = nome + cognome + colore + 21

e162c30bd245bb41bacdfb2d7c419c229b06d5ed
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-07 16:07:21 +0200

    add password generator

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome = prompt("Scrivi il tuo nome");
+var cognome = prompt("Scrivi il tuo cognome");
+var colore = prompt("Scrivi il tuo colore preferito");
+
+
+
+document.getElementById("myH1").innerHTML = nome + cognome + colore + 21
